450c98f265a6c5ca26eb65b764f03937eebe28b9
Misha Pateev <tokensmix@gmail.com>
2020-10-01 14:06:31 +0300

    feat: did task towlsort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (arr) {
+    let newArr = [];
+    if(!arr) { return newArr}
+    for(let i = 0; i < arr.length; i++){
+      if(i % 2 !== 0){
+          arr[i].sort((a, b) => b - a);
+        }
+        for(let j = 0; j < arr[i].length; j++){
+          newArr = newArr.concat(arr[i][j]);
+        }
+        
+    }
+    return newArr;
 }
